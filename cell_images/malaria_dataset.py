b1dfc058c45e4279ca00ef4f410c0888d92e1312
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-05-01 14:15:17 +0900

    enabling malaria dataset to support shuffle

--- cell_images/malaria_dataset.py
+++ cell_images/malaria_dataset.py
@@ -9,7 +9,7 @@ import random
 class MalariaImageLabelDataset(Dataset):
     """A dataset class to retrieve samples of paired images and labels"""
 
-    def __init__(self, transform=None):
+    def __init__(self, transform, shuffle=None):
         """
         Args:
             csv (string): Path to the csv file with data
@@ -18,11 +18,19 @@ class MalariaImageLabelDataset(Dataset):
                                             on a sample.
         """
         super().__init__()
-        self.parasitized_path = '../input/cell_images/cell_images/Parasitized/'
-        self.uninfected_path = '../input/cell_images/cell_images/Uninfected/'
-        self.infected = os.listdir(self.parasitized_path)
-        self.uninfected = os.listdir(self.uninfected_path)
+        FILE_ABSOLUTE_PATH = os.path.abspath(__file__)
+        cell_images_folder_path = os.path.dirname(FILE_ABSOLUTE_PATH)
+        included_extensions = ['jpg', 'jpeg', 'png']
+        self.infected_path = cell_images_folder_path + '/Parasitized/'
+        self.uninfected_path = cell_images_folder_path + '/Uninfected/'
+        self.infected_paths  = [self.infected_path + fn for fn in os.listdir(self.parasitized_path)
+              if any(fn.endswith(ext) for ext in included_extensions)]
+        self.uninfected_paths = [self.uninfected_path + fn for fn in os.listdir(self.uninfected_path)
+              if any(fn.endswith(ext) for ext in included_extensions)]
+        self.data = self.infected_paths + self.uninfected_paths
         self.transform = transform
+        if shuffle:
+            random.shuffle(self.data)
 
     def __len__(self):
         return len(self.infected) + len(self.uninfected)
@@ -31,14 +39,13 @@ class MalariaImageLabelDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        path = self.infected + self.infected if idx < len(self.infected) else self.parasitized_path + self.uninfected
-        label = 'parasitized' if idx < len(self.infected) else 'uninfected'
+        path = self.data[idx]
+        label = 1 if path.startswith(self.infected_path) else 0
         image = Image.open(path)
 
         if self.transform:
             image = self.transform(image)
 
-        sample = {'image': image, 'label': label}
+        sample = (image, label)
 
         return sample
-
