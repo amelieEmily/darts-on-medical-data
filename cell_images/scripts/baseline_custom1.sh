4c220bcc8760efd9883b771b238464b25427dcff
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-05-16 00:51:22 +0900

    activating python environment

--- cell_images/scripts/baseline_custom1.sh
+++ cell_images/scripts/baseline_custom1.sh
@@ -6,6 +6,7 @@
 export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
 source activate
 source /vol/cuda/10.0.130/setup.sh
+source /vol/bitbucket/ys3316/darts-on-medical-data/cell_images/venv/bin/activate
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
